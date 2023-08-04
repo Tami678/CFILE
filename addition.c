a23a9a70f2d71406949d91a2fefc69a48ef955ce
Tanzim akhtar <talam8068@gmail.com>
2023-08-04 21:19:48 +0530

    Second Commit

---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int mian()
+{
+    int a,b,c,sum;
+    printf("enter the valu of a and b");
+    scanf("%d \n %d",&a,&b);
+    sum = a+b;
+    printf("the sum of the two number is %d",sum);
+
+}
